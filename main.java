ea3bb2edb7d6a1d3a70be6b540cd22396d7aadcf
cavi2024 <168128362+cavi2024@users.noreply.github.com>
2024-05-27 18:38:37 +0200

    Update main.java
    Uploading my homework from the first lesson.

--- main.java
+++ main.java
@@ -1,1 +1,20 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
+public class Main {
+
+    public static void main(String[] args) {
+        String seller;
+        seller = "Jméno a příjmení prodejce";
+        LocalDate birthDateOfSeller;
+        int quantityOfContracts = 3;
+        double totalQuantityOfCarrot = 205.4; String tq; tq = "t";
+        String nameOfTownOfSeller = "jméno města";
+        String vehicleRegistrationPlate = "SPZ";
+        double consumptionOfFuel; String f ="l/100km";
+        String adressOfPC = "IPv4";
+
+        System.out.println("Průměrné množství prodané mrkve na 1 smlouvu: "+ totalQuantityOfCarrot/quantityOfContracts+" t.");
+
+    }
+}
